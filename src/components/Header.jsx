2f46588db4a1574f35b846ac6af3812ddfb9863e
Ksenia Vislova <vislova.fortech@gmail.com>
2022-09-19 10:46:38 +0300

    refactor: add margin-botton for title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Typography } from "../styles/component";
 
 export const Header = () => {
   return (
-    <Typography variant="h1" component="h1">
+    <Typography variant="h1" component="h1" sx={{ mb: 4 }}>
       Pokemons
     </Typography>
   );
